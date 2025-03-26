98f925c6e751f1f71a60b2feea97d11dee02921e
Vi Le <lecongvi18052002@gmail.com>
2025-03-26 13:34:49 +0700

    update

--- internal/configs/config.go
+++ internal/configs/config.go
@@ -4,27 +4,38 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/lcv-back/goload/configs"
 	"gopkg.in/yaml.v2"
 )
 
 type ConfigFilePath string
 
 type Config struct {
+	GRPC     GRPC     `yaml:"grpc"`
+	HTTP     HTTP     `yaml:"http"`
 	Log      Log      `yaml:"log"`
 	Auth     Auth     `yaml:"auth"`
 	Database Database `yaml:"database"`
+	Cache    Cache    `yaml:"cache"`
 }
 
 func NewConfig(filePath ConfigFilePath) (Config, error) {
-	configBytes, err := os.ReadFile(string(filePath))
-	if err != nil {
-		return Config{}, fmt.Errorf("failed to read yaml file: %w", err)
+	var (
+		configBytes = configs.DefaultConfigBytes
+		config      = Config{}
+		err         error
+	)
+
+	if filePath != "" {
+		configBytes, err = os.ReadFile(string(filePath))
+		if err != nil {
+			return Config{}, fmt.Errorf("failed to read YAML file: %w", err)
+		}
 	}
 
-	config := Config{}
 	err = yaml.Unmarshal(configBytes, &config)
 	if err != nil {
-		return Config{}, fmt.Errorf("failed to unmarshal yaml: %w", err)
+		return Config{}, fmt.Errorf("failed to unmarshall YAML: %w", err)
 	}
 
 	return config, nil
